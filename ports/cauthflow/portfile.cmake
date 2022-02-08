1851580b810dc5b07d9b335b62c77f0e6d4f83e2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-08 07:31:21 -0500

    [ports\cauthflow\portfile.cmake] Disable tests

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS     "-DBUILD_TESTING=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
