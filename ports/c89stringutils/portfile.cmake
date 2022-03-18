ec1903e77972709bdc55615b6195be5da3c94460
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-18 17:35:50 -0400

    [ports/c89stringutils/portfile.cmake] Re-add `asprintf; change cmake option name

--- ports/c89stringutils/portfile.cmake
+++ ports/c89stringutils/portfile.cmake
@@ -1,15 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            offscale/c89stringutils
-    REF             8beb386ceef486b921cba500cf5489cc5e7f1474
-    SHA512          7e9bec9cb10cd86e7e67afd189ea04d0964339036d5a7663e1eeba5ae8c0db0c239590cc8461397f1488a185dbda1b8137a62e07e50405b40986b6366d7521a5
+    REF             aa7f8ac83a324f520425cfe9a52847b4ecfa9e48
+    SHA512          3f03df44fa8a3ca974ed513ec0d5a4b41323d813e07a2c99b69336779c6cee6be4b1ba66174deaa13addf2eec8e7cfae19d6a92f7b177aa459e21c664d97d0a8
     HEAD_REF        master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        "-DBUILD_TESTS=OFF"
+        "-DBUILD_TESTING=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
