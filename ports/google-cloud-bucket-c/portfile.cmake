58b6e3c8eb50e068f0f9114230122db5f67a1c07
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-07-16 00:42:53 -0400

    Merge google-cloud-bucket-c

--- ports/google-cloud-bucket-c/portfile.cmake
+++ ports/google-cloud-bucket-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             39fd6b0a40524244b20644ab7e1b8d1da0f0e2d6
-    SHA512          a1af4cd318d9cf040f8e71faf5288151f173ea3a5f72f7dfa87434c603fb35c350217c0df8e891a8cb3ab3c022fd8fd86a0230c01b474f8d4eea44c63ff76bf9
+    REF             a456061176fd47d7ec34eb9e1c1c72adfdbb75dd
+    SHA512          bd13e650ab61665ef3f65e9cbff7ab045de64a8fbebc1cc4eb0a06fd6990a3e1f6cf88af7d85475626e4ddd3b3e5ab98f335d4618ee5544b135f8c3b4f5c882a
     HEAD_REF        master
 )
 
