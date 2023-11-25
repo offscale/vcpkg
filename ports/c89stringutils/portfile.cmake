2044080de9c5c2074b89c948773ff0b592d8ec48
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-11-25 13:37:17 -0500

    [ports/c89stringutils] Upgrade to latest from branch

--- ports/c89stringutils/portfile.cmake
+++ ports/c89stringutils/portfile.cmake
@@ -1,23 +1,19 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO offscale/c89stringutils
-    REF "${VERSION}"
-    SHA512 82edb341d5566c42eaffcd5c87d4fbd82a4e47b9c31a8533b08d28b9e1311ced281b59b3b6103e274355a82117095fcff1cb5f9c29eecc9563dc3cd962a37773
-    HEAD_REF master
-    PATCHES
-        no_flags.patch
+    REPO            offscale/c89stringutils
+    REF             a9564ebdade0de40a733a1d234af18732a87e97b
+    SHA512          dee9b0fa066d6849ed6d8bcfa4936b32dca0449b99cb87070628e4818f97ea61e81f07edcc0177f72deec5b917bca8a909783173072aa7354df516095c6d9cdf
+    HEAD_REF        master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_TESTING=OFF
+        "-DBUILD_TESTING=OFF"
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
-vcpkg_copy_pdbs()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE-APACHE" "${SOURCE_PATH}/LICENSE-MIT")
+file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/c89stringutils"
+     RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share")
