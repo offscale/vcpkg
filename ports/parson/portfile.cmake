28ee1e9608f519b906c4bd9e6243f228d70df2e7
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-04-30 11:59:45 +1200

    Upgrade SamuelMarks and offscale ports

--- ports/parson/portfile.cmake
+++ ports/parson/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            SamuelMarks/parson
-    REF 62e9264e0e3acef37099067291cf03209823a443 # See commit message for version number
-    SHA512 625eb2da39da892621c6830e954730fd22239e9c06f90f2aa11cfd00d7483c1cbd6d0a192b58cec25c2cc9a3dda596a6a73f71af2d57f9e91d04e1158f3cfb34
+    REF de49ada50a9feab197df0b717477252ef6a17738 # See commit message for version number
+    SHA512 8afdbc1051ccf0e4f75ed9a4eb2cec7763ff6f34b04a6f814ff14bae4fbea02d0371b3a9250cf537732c9a7dbe961053f2ade9db6a8f57c40bc5b4e92da030a0
     HEAD_REF master
     PATCHES
         fix-cmake-files-path.patch
@@ -12,6 +12,8 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DPARSON_SINGLE_HEADER=OFF
 )
 
 vcpkg_cmake_install()
